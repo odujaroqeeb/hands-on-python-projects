a8a7c36db23360d3ef5a32efc1855eab078c3623
odujaroqeeb <odujaroqeeb@outlook.com>
2026-07-09 23:16:06 +0100

    Create menu_system project

---
+++ 05_menu_system/main.py
@@ -0,0 +1,25 @@
+print("Hello!, Welcome to your menu system.")
+
+name = input("Enter your name? ")
+
+print(f"Hello! {name}")
+
+menu = ["1. Say Hello", "2. Tell a joke", "3. Exit"]
+
+while True:
+    for i in menu:
+        print(i)
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("Hello!!!!")
+        print()  # To leave a space between the output and the menu
+    elif choice == 2:
+        print("A fool will laugh when nothing is funny.")
+        print()  # To leave a space between the output and the menu
+    elif choice == 3:
+        print(f"Thank you {name} for using the menu system. Goodbye!")
+        break
+    else:
+        print("Invalid input")
